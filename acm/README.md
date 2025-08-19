f6f60fc1bba668020826fff892600046e4ac77b5
akynazh <akynazh@gmail.com>
2025-08-19 18:16:18 +0800

    feat: pro

--- acm/README.md
+++ acm/README.md
@@ -3,7 +3,7 @@
 ## Problems
 
 - pro0705e: Automated Parking Lot - 模拟系统
-- pro164c: Cafe - 模拟系统
+- pro164c: Cafe - 模拟系统 & 事件驱动
 - pro165c: Notepad - 模拟系统
 - pro166c: KNN - 网格索引
 - pro167c: Mineral Collect - 并查集 & 网格索引
@@ -16,7 +16,7 @@
 - pro0725e: Electric Vehicle Travel - 迪杰斯特拉 & 状态转移
 - pro2512t: Airport Parking - 模拟系统 & 延迟标记
 - pro0802e: Smart Farm - 网格索引 & 延迟标记
-- pro171c: Production System
+- pro171c: Production System - 模拟系统 & 事件驱动
 
 ## 2025 SW Test Schedule (July, August)	
 
--- acm/pro171c/Main.java
+++ acm/pro171c/Main.java
@@ -5,6 +5,112 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class UserSolution {
+    static class Product {
+        int pId;
+        int mLine;
+        int mTimeProcess;
+        int equipId;
+        int status;
+        int endTime;
+
+        public Product(int pId, int mLine, int mTime, int equipId) {
+            this.pId = pId;
+            this.mLine = mLine;
+            this.mTimeProcess = mTime;
+            this.equipId = equipId;
+            status = 1;
+        }
+    }
+
+    static class Line {
+        int idLine;
+        int idProduct;
+        boolean producing;
+        LinkedList<Product> listProduct;
+
+        public Line(int idLine) {
+            this.idLine = idLine;
+            listProduct = new LinkedList<>();
+        }
+
+    }
+
+    Line[] listLine;
+    boolean[] isUsedEquipment;
+    HashMap<Integer, Product> hashProduct;
+    PriorityQueue<Product> heapProduction;
+    int L, M;
+
+    void init(int L, int M) {
+
+        this.L = L;
+        this.M = M;
+        listLine = new Line[L];
+        for (int i = 0; i < L; i++) {
+            listLine[i] = new Line(i);
+        }
+        isUsedEquipment = new boolean[M];
+        hashProduct = new HashMap<>();
+        heapProduction = new PriorityQueue<>((o1, o2) -> {
+            if (o1.endTime == o2.endTime) {
+                return o1.mLine - o2.mLine;
+            }
+            return o1.endTime - o2.endTime;
+        });
+    }
+
+    public void syncTime(int time) {
+        while (!heapProduction.isEmpty() && heapProduction.peek().endTime <= time) {
+            Product product = heapProduction.peek();
+            int curTime = product.endTime;
+            // 根据某条生产线完成的时间来进行更新
+            while (!heapProduction.isEmpty() && heapProduction.peek().endTime == curTime) {
+                product = heapProduction.poll();
+                product.status = 3;
+                listLine[product.mLine].producing = false;
+                isUsedEquipment[product.equipId] = false;
+            }
+            // 上次提交的时间 <= 现在任何生产线完成生产的时间
+            for (int i = 0; i < L; i++) {
+                updateLine(listLine[i], curTime);
+            }
+        }
+    }
+
+    public void updateLine(Line line, int end) {
+        if (!line.listProduct.isEmpty() && !line.producing && !isUsedEquipment[line.listProduct.peek().equipId]) {
+            Product product = line.listProduct.poll();
+            product.endTime = product.mTimeProcess + end;
+            product.status = 2;
+            heapProduction.add(product);
+            line.producing = true;
+            isUsedEquipment[product.equipId] = true;
+            line.idProduct = product.pId;
+        }
+    }
+
+    int request(int tStamp, int pId, int mLine, int eId, int mTime) {
+        // 每次提交前，更新所有生产线的状态
+        syncTime(tStamp - 1);
+        Product product = new Product(pId, mLine, mTime, eId);
+        hashProduct.put(pId, product);
+        Line line = listLine[mLine];
+        line.listProduct.add(product);
+        syncTime(tStamp);
+        updateLine(line, tStamp); // tStamp 下没有事件完成
+        return line.producing ? line.idProduct : -1;
+    }
+
+    int status(int tStamp, int pId) {
+        Product product = hashProduct.get(pId);
+        if (product == null)
+            return 0;
+        syncTime(tStamp);
+        return product.status;
+    }
+}
+
+class UserSolutionMine {
     int L, M, tk;
     Map<Integer, Product> products;
     Queue<Product>[] lines;
