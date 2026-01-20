43a3d21bf4ce8c3781b98c79e7102fe5dfa36519
akynazh <akynazh@gmail.com>
2026-01-20 17:58:59 +0800

    feat: exp

---
+++ acm/exp/2025_12_delivery_rider/user.cpp
@@ -0,0 +1,16 @@
+/*
+typedef struct {
+	int y, x;
+}Coordinates;
+
+typedef struct {
+	Coordinates src;
+	Coordinates dest;
+} Delivery;
+
+extern bool deliver(int mID);
+*/
+void process(Coordinates mRider, Delivery mDeliveries[])
+{
+
+}
